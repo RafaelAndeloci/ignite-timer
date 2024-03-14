61c04cfb15a2169de5cd555e0a81be54d33c3d5d
rafael <rafaelandeloci67@gmail.com>
2024-03-14 07:44:59 -0300

    aaaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Button } from "./components/Button";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
+      <BrowserRouter>
+        <Router></Router>
+      </BrowserRouter>
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
