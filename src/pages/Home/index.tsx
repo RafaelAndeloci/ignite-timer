26894b9a61b25949ef67e560628da43579e78292
Rafael Andeloci Rodrigues Gonçalves <rafaelandeloci67@gmail.com>
2024-03-14 13:58:36 -0300

    comit do trabaio

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,60 @@
+import { Play } from "phosphor-react";
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em </label>
+          <TaskInput
+            list="task-suggestions"
+            placeholder="Dê um nome ao seu projeto"
+            type="text"
+            id="task"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="projeto 1"></option>
+            <option value="projeto 2"></option>
+            <option value="projeto 3"></option>
+            <option value="Banana"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            min={5}
+            max={60}
+            step={5}
+            placeholder="00"
+            type="number"
+            id="minutesAmount"
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountDownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  );
+}
